d19aa60c36ab6247fe4e896af25e0c74de5351d3
Yi Chao <changingtimes@gmail.com>
2020-06-11 19:52:19 -0500

    finished implementing update

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Body {
     double xxPos;
     double yyPos;
@@ -5,6 +7,8 @@ public class Body {
     double yyVel;
     double mass;
     String imgFileName;
+    static final double G = 6.67e-11;
+
     public Body(double xP, double yP, double xV,
                 double yV, double m, String img) {
         this.xxPos = xP;
@@ -23,4 +27,88 @@ public class Body {
         this.mass = b.mass;
         this.imgFileName = b.imgFileName;
     }
+
+    private double calcDistanceSq(double x, double y) {
+        double dist = Math.abs(x - y);
+
+        return dist * dist;
+    }
+
+    public double calcDistance(Body b) {
+        double xSquared = this.calcDistanceSq(this.xxPos, b.xxPos);
+        double ySquared = this.calcDistanceSq(this.yyPos, b.yyPos);
+
+        return Math.sqrt(xSquared + ySquared);
+    }
+
+    public double calcForceExertedBy(Body b) {
+        double numerator = this.G * this.mass * b.mass;
+        double distance = this.calcDistance(b);
+
+        return numerator / Math.pow(distance, 2);
+    }
+
+
+    public double calcForceExertedByX(Body b) {
+        double force = this.calcForceExertedBy(b);
+        double distance = this.calcDistance(b);
+
+        if( this.xxPos > b.xxPos) {
+            return (force / distance) * (-(b.xxPos - this.xxPos));
+        }
+
+        return (force / distance) * (b.xxPos - this.xxPos);
+    }
+
+    public double calcForceExertedByY(Body b) {
+        double force = this.calcForceExertedBy(b);
+        double distance = this.calcDistance(b);
+
+        if( this.yyPos > b.yyPos) {
+            return (force / distance) * (-(b.yyPos - this.yyPos));
+        }
+
+        return (force / distance) * (b.yyPos - this.yyPos);
+    }
+
+    public double calcNetForceExertedByX(Body[] bodies) {
+        double output = 0;
+        Body currentBody = this;
+        for(int i = 0; i < bodies.length; i++) {
+            currentBody = bodies[i];
+            if(!currentBody.equals(this)) {
+                output += this.calcForceExertedByX(currentBody);
+            } else {
+                output += 0;
+            }
+        }
+        return output;
+    }
+
+    public double calcNetForceExertedByY(Body[] bodies) {
+        double output = 0;
+        Body currentBody = this;
+        for(int i = 0; i < bodies.length; i++) {
+            currentBody = bodies[i];
+            if(!currentBody.equals(this)) {
+                output += this.calcForceExertedByY(currentBody);
+            } else {
+                output += 0;
+            }
+        }
+        return output;
+    }
+
+    public void update(double time, double xForce, double yForce) {
+        double xAccel = xForce / this.mass;
+        double yAccel = yForce / this.mass;
+
+        this.xxVel += xAccel * time;
+        this.yyVel += yAccel * time;
+
+        this.xxPos += this.xxVel * time;
+        this.yyPos += this.yyVel * time;
+    }
+
+
 }
