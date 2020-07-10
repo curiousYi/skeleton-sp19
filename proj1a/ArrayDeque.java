bdcb1852c929557687296f76ebeec7832109235b
Yi Chao <changingtimes@gmail.com>
2020-07-10 08:36:49 -0500

    Saving project1a progress

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,122 @@
+public class ArrayDeque<Item> {
+    private Node<Item> sentinel;
+    private int size;
+    private Node<Item> last;
+
+    public ArrayDeque() {
+        sentinel = new Node("asdfsdf");
+        last = sentinel;
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    public Item get (int count) {
+        Node<Item> start = sentinel.next;
+        while(count != 0) {
+            start = start.next;
+            count--;
+        }
+        return start.item;
+    }
+
+    public int size () {
+        return size;
+    }
+
+    public class Node<Item> {
+        public Item item;
+        public Node<Item> next;
+        public Node<Item> prev;
+
+        public Node(Item i) {
+            item = i;
+        }
+
+    }
+
+    public void addFirst(Item thing) {
+        Node<Item> tempNode = sentinel.next;
+        Node<Item> newFirst = new Node(thing);
+
+        newFirst.next = tempNode;
+        newFirst.prev = sentinel;
+
+        sentinel.next = newFirst;
+        tempNode.prev = newFirst;
+
+        if(last == tempNode) {
+            last = newFirst;
+        }
+
+        size = size + 1;
+    }
+
+    public void addLast(Item thing){
+        Node<Item> tempNode = last;
+        Node<Item> newLast = new Node(thing);
+
+        newLast.next = sentinel;
+        newLast.prev = last;
+
+        sentinel.prev = newLast;
+        tempNode.next = newLast;
+
+        last = newLast;
+        size = size + 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printDeque() {
+        String thing = "";
+        Node<Item> tracker = sentinel.next;
+
+        for(int i =0; i < size; i++) {
+            thing += tracker.item.toString() + " ";
+            tracker = tracker.next;
+        }
+
+        System.out.println(thing);
+    }
+
+    public Item removeFirst() {
+        if(sentinel.next == sentinel) {
+            return null;
+        }
+
+        Node<Item> tempNode = sentinel.next;
+
+        sentinel.next = sentinel.next.next;
+        sentinel.next.next.prev = sentinel;
+
+        tempNode.next = null;
+        tempNode.prev = null;
+        size = size - 1;
+
+        return tempNode.item;
+    }
+
+    public Item removeLast() {
+        if(last == sentinel) {
+            return null;
+        }
+
+        Node<Item> tempNode = last;
+
+        last = last.prev;
+        last.next = sentinel;
+        sentinel.prev = last;
+
+        tempNode.next = null;
+        tempNode.prev = null;
+        size = size - 1;
+
+        return tempNode.item;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+    }
+}
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,136 @@
+/** Performs some basic linked list tests. */
+public class ArrayDequeTest {
+
+	/*
+		(arr, 10)
+		10^10 1's will be added
+	 */
+	public static long addFirstTest(ArrayDeque arr, double power) {
+		double count = Math.pow(10, power);
+		long startTime = System.currentTimeMillis();
+
+		for(int i=1; i <= count; i++) {
+			arr.addFirst(1);
+		}
+
+		long endTime = System.currentTimeMillis();
+
+		return endTime - startTime;
+	}
+
+	public static void checkInitialSizeTest() {
+		ArrayDeque<String> arr = new ArrayDeque();
+
+		boolean passed = arr.size() == 8;
+		printTestStatus(passed);
+
+	}
+
+	public static void checkProportionalTest() {
+		//check whether the array is proportional to the actual number of items
+		ArrayDeque<String> arr = new ArrayDeque();
+
+		//could make this math.rand but it might be less robust
+		double count = Math.pow(10, 2);
+
+		for(int i=1; i <= count; i++) {
+			arr.addFirst("foo");
+		}
+
+		int size = arr.size();
+
+		for(int i=1; i <= count; i++) {
+			arr.removeFirst();
+		}
+		int sizeAfterRemoval = arr.size();
+
+		//assuming it's a multiple of the initial size
+		boolean passed;
+
+		if(sizeAfterRemoval == 0) {
+			passed = false;
+		} else {
+			passed = size % sizeAfterRemoval == 0;
+		}
+		printTestStatus(passed);
+	}
+
+	//check starting size of array is 8
+
+	//Check whether it does methods in O(n) time
+		//add
+
+	//Optional
+		//get
+		//remove
+		//size
+	//Verify for something else
+	/* Prints a nice message based on whether a test passed.
+	 * The \n means newline. */
+	public static void printTestStatus(boolean passed) {
+		if (passed) {
+			System.out.println("Test passed!\n");
+		} else {
+			System.out.println("Test failed!\n");
+		}
+	}
+
+	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
+	  * finally printing the results. 
+	  *
+	  * && is the "and" operation. */
+//	public static void addIsEmptySizeTest() {
+//		System.out.println("Running add/isEmpty/Size test.");
+//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+//
+//		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+//
+//		boolean passed = checkEmpty(true, lld1.isEmpty());
+//
+//		lld1.addFirst("front");
+//
+//		// The && operator is the same as "and" in Python.
+//		// It's a binary operator that returns true if both arguments true, and false otherwise.
+//		passed = checkSize(1, lld1.size()) && passed;
+//		passed = checkEmpty(false, lld1.isEmpty()) && passed;
+//
+//		lld1.addLast("middle");
+//		passed = checkSize(2, lld1.size()) && passed;
+//
+//		lld1.addLast("back");
+//		passed = checkSize(3, lld1.size()) && passed;
+//
+//		System.out.println("Printing out deque: ");
+//		lld1.printDeque();
+//
+//		printTestStatus(passed);
+//
+//	}
+
+	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
+//	public static void addRemoveTest() {
+//
+//		System.out.println("Running add/remove test.");
+//
+//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+//		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
+//		// should be empty
+//		boolean passed = checkEmpty(true, lld1.isEmpty());
+//
+//		lld1.addFirst(10);
+//		// should not be empty
+//		passed = checkEmpty(false, lld1.isEmpty()) && passed;
+//
+//		lld1.removeFirst();
+//		// should be empty
+//		passed = checkEmpty(true, lld1.isEmpty()) && passed;
+//
+//		printTestStatus(passed);
+//	}
+
+	public static void main(String[] args) {
+		System.out.println("Running tests.\n");
+		checkInitialSizeTest();
+		checkProportionalTest();
+	}
+} 
