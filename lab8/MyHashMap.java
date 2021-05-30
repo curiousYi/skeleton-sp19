a8326ac1bf15af567582429785f2dfb5c3133ef9
Yi Chao <changingtimes@gmail.com>
2021-05-30 10:05:33 -0700

    Fixed one broken test. TO-DO get remaining broken tests to run

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -73,7 +73,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         LinkedList<Entry> llRef;
         Entry ref;
         //map old buckets
-        for(int i = 0; i < buckets.length; i++) {
+        for(int i = 0; i < oldBuckets.length; i++) {
             llRef = oldBuckets[i];
             if(llRef != null) {
                 for (int j = 0; j < llRef.size(); j++) {
