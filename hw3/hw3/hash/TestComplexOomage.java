4f438dc6fad84601c8a47c5a3c21a6680fa9da0d
Yi Chao <changingtimes@gmail.com>
2021-06-22 09:01:33 -0700

    Did the complex oomage and deadly oomage part which was due to integer overflow. Tried using the random num generator but it didn't work. However got it working with some example off the internet.

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -20,7 +20,7 @@ public class TestComplexOomage {
     /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
        is correct. This is true even though our given ComplexOomage class
        has a flawed hashCode. */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -30,20 +30,40 @@ public class TestComplexOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /* TODO: Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
         // Your code here.
-
+        for(int i = 0; i < 1000; i++) {
+            List<Integer> killerItems = new ArrayList<>();
+            for(int j = 0; j < i + 1; j++) {
+                killerItems.add(255);
+            }
+            deadlyList.add(new ComplexOomage(killerItems));
+        }
+        /* For visualization of oomages whose hashcodes are 0 because of int overflow. */
+//        double scale = 1.0;
+//        HashTableDrawingUtility.setScale(scale);
+//        visualize(deadlyList, 10, scale);
+//
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+//        List<Oomage> deadlyList = new ArrayList<>();
+//        //idea is to have 7 or 8 parmams minimum
+//
+//        int N = 10000;
+//
+//        for (int i = 0; i < N; i += 1) {
+//            deadlyList.add(ComplexOomage.randomBadParamsComplexOomage());
+//        }
+//
+//        assertFalse(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
